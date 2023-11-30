1563217cd1181187aec07a805f8ed7f8e6ddf78c
BaptisteCMY <baptiste.comoy2501@gmail.com>
2023-11-30 17:15:14 +0100

    Fin accueil

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -8,41 +8,103 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("RescueMate"),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            "assets/img/logoRM.png",
+            width: 30,
+            height: 30,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        title: const Text('RescueMate',
+        style: TextStyle(
+          fontSize: 28,
+          color: Colors.black
+        ),),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              // Code à exécuter lorsque le bouton du menu est pressé
+            },
+          ),
+        ],
+        // Increase the preferredSize
+        toolbarHeight: 60,
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Image.asset('assets/img/logoRM.png', width: 150),
-            const Text('RescueMate', style: TextStyle(fontSize: 42, fontFamily: 'Poppins')),
-            const Text("Un clic pour sauver une vie !",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
-            ),
+            const SizedBox(height: 40),
+            const Text('URGENCE', style: TextStyle(fontSize: 42, fontFamily: 'Poppins')),
             const Padding(padding: EdgeInsets.all(20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // Align buttons horizontally
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 10)), // Adjust the padding
+                    backgroundColor: MaterialStateProperty.all(const Color(0xFF97A2FF)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (_, __, ___) => const NeedHelp(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "J'ai besoin\n d'aides",
+                    style: TextStyle(fontSize: 18, color: Colors.black54),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(width: 40), // Add some space between buttons
+                ElevatedButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 10)), // Adjust the padding
+                    backgroundColor: MaterialStateProperty.all(const Color(0xFFA6FF97)),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (_, __, ___) => const NeedHelp(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Premiers\n Secours",
+                    style: TextStyle(fontSize: 18, color: Colors.black54),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
             ElevatedButton(
-              style: const ButtonStyle(
-                padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
-                backgroundColor: MaterialStatePropertyAll(Color(0xFF97A2FF)),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(const EdgeInsets.only(left: 120, right: 120, top: 20, bottom: 20)), // Adjust the padding
+                backgroundColor: MaterialStateProperty.all(const Color(0xFFFF856A)),
               ),
               onPressed: () {
                 Navigator.push(
                   context,
                   PageRouteBuilder(
-                      pageBuilder: ( _, __, ___) => NeedHelp()
+                    pageBuilder: (_, __, ___) => const NeedHelp(),
                   ),
                 );
               },
-              child:  const Text("J'ai besoin d'aides",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black45
-                ),
+              child: const Text(
+                "Urgence",
+                style: TextStyle(fontSize: 18, color: Colors.black54),
+                textAlign: TextAlign.center,
               ),
-            )
+            ),
           ],
         ),
       ),
